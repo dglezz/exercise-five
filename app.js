ae7f812c2dac5213881dcaab57b5f66fff47e78b
Dariana Gonzalez <100489203+dglezz@users.noreply.github.com>
2024-11-05 09:38:19 -0500

    querying posts

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const firebase = require("firebase/app");
 const app = express();
 const port = 3000;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDubFt4j7WX6H9hDsTZxUB4acUTPBSg6RU",
+  authDomain: "dg-exercise-five-fall-2024.firebaseapp.com",
+  projectId: "dg-exercise-five-fall-2024",
+  storageBucket: "dg-exercise-five-fall-2024.firebasestorage.app",
+  messagingSenderId: "63670923104",
+  appId: "1:63670923104:web:a47f0e2fbc886ccd46f805",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require("./routes/index.js");
 const createPostRoute = require("./routes/createPost.js");
--- routes/singlePost.js
+++ routes/singlePost.js
@@ -1,8 +1,35 @@
+// import express
 const express = require("express");
+// define a router
 const router = express.Router();
+// initialize firestore
+const firestore = require("firebase/firestore");
+// create a reference to the database
+const db = firestore.getFirestore();
 
 router.get("/", (request, response) => {
-  response.send("Single Post Route");
+  response.send("Please include a post ID");
+});
+
+router.get("/:postId", (request, response) => {
+  //get post id from params
+  const postId = request.params.postId;
+  // get reference to firestore collection with postId as a argument
+  const postQuery = firestore.getDoc(firestore.doc(db, "posts", postId));
+
+  // Query data by the post and send it to the browser
+  postQuery
+    .then((res) => {
+      const post = res.data();
+      if (!post) response.send("Data does not exist");
+      response.send(post);
+    })
+    .catch((error) => {
+      console.log(error);
+      return response.send(error);
+    });
+
+  //   response.send("Post: ");
 });
 
 module.exports = router;
